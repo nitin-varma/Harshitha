3fc36013978f08844e3d875ca3a1fc6750bbaf30
nitin-varma <51487712+nitin-varma@users.noreply.github.com>
2022-06-18 12:56:18 +0530

    Fixed an Error

--- Factorial.java
+++ Factorial.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Factorial{
     public staitc void main(String[] args){
         Scanner scan = new Scanner(system.in);
-        int n = scan.nextInt()l
+        int n = scan.nextInt();
         int res=1;
         for(int i=1;i<=n;i++)
         res*=i;
